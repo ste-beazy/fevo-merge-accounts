ba8578fa7f919411f9c860c1b0404f17c29e2db6
carmen <carmen@carmens-Mac-mini.local>
2023-01-05 14:48:36 -0500

    refactor: remove unnecessary 'accountsMatch' fn
    ambition got the best of me when i tried to match every value, when i realized
    all that is important is that the accounts have ONE matching email in common...
    not every value. this removes the function (which was shaky anyway) and strengthens
    the finding of existing account indices to merge by checking the emails alone

--- src/tools/tools.ts
+++ src/tools/tools.ts
@@ -4,27 +4,10 @@ export const emailsMatch = (emails: string[], email: string): boolean => {
   return !!emails.find((e) => e === email)
 }
 
-export const accountsMatch = (object: MergedAccount, objectToCompare: MergedAccount): boolean => {
-const keys = Object.keys(object)
-const keysToMatch = Object.keys(objectToCompare)
-const values = Object.values(object)
-const valuesToMatch = Object.values(objectToCompare)
-const keysMatch = keys.filter((key) => keysToMatch.includes(key)).length === keys.length
-const valuesMatch = values.filter((value) => {
-  if (Array.isArray(value)) {
-    const arrayValues = valuesToMatch.filter((val) => Array.isArray(val))
-    return !arrayValues.find((array) => array.includes(value))
-  }
-  return valuesToMatch.includes(value)
-}).length === values.length
-
-return keysMatch && valuesMatch
-}
-
-export const findExistingAccountIndexes = (accounts: MergedAccount[], existingAccountList: MergedAccount[]): number[] => {
-return existingAccountList.map((existingAccount, index) => {
-  const match = accounts.find((acc) => accountsMatch(acc, existingAccount))
-  if (match) return index
-  return -1
-})
+export const findExistingAccountIndexes = (accounts: MergedAccount[], mergedAccountList: MergedAccount[]): number[] => {
+  return mergedAccountList.map((mergedAccount, index) => {
+    const match = mergedAccount.emails.find((email) => accounts.find((acc) => emailsMatch(acc.emails, email)))
+    if (match) return index
+    return -1
+  })
 }
